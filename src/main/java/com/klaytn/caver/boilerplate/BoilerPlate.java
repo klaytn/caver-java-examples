6046e406917fcf24114cf5c22d3d051fc3f0168e
kale.kim <kale.kim@groundx.xyz>
2021-03-17 11:33:01 +0900

    modify a test code

--- src/main/java/com/klaytn/caver/boilerplate/BoilerPlate.java
+++ src/main/java/com/klaytn/caver/boilerplate/BoilerPlate.java
@@ -1,10 +1,22 @@
 package com.klaytn.caver.boilerplate;
 
 import com.klaytn.caver.Caver;
+import com.klaytn.caver.account.Account;
+import com.klaytn.caver.methods.response.AccountKey;
+import com.klaytn.caver.methods.response.Bytes32;
+import com.klaytn.caver.methods.response.TransactionReceipt;
+import com.klaytn.caver.transaction.response.PollingTransactionReceiptProcessor;
+import com.klaytn.caver.transaction.response.TransactionReceiptProcessor;
+import com.klaytn.caver.transaction.type.AccountUpdate;
+import com.klaytn.caver.transaction.type.ValueTransfer;
+import com.klaytn.caver.wallet.keyring.KeyringFactory;
+import com.klaytn.caver.wallet.keyring.SingleKeyring;
 import okhttp3.Credentials;
+import org.web3j.protocol.exceptions.TransactionException;
 import org.web3j.protocol.http.HttpService;
 
 import java.io.IOException;
+import java.math.BigInteger;
 
 public class BoilerPlate {
     private static final String URL_NODE_API = "https://node-api.klaytnapi.com/v1/klaytn";
@@ -25,10 +37,58 @@ public class BoilerPlate {
     }
 
     public static void test(Caver caver) {
-        //Copy and pasted test code here.
+        String testAddress = "0x{address}";
+        String testPrivateKey = "0x{private key}";
+
+        SingleKeyring keyring = (SingleKeyring) KeyringFactory.create(testAddress, testPrivateKey);
+        caver.wallet.add(keyring);
+
+        String newKey = KeyringFactory.generateSingleKey();
+        SingleKeyring newKeyring = KeyringFactory.create(testAddress, newKey);
+        Account account = newKeyring.toAccount();
+
+        AccountUpdate accountUpdate = new AccountUpdate.Builder()
+                .setKlaytnCall(caver.rpc.klay)
+                .setFrom(testAddress)
+                .setAccount(account)
+                .setGas(BigInteger.valueOf(50000))
+                .build();
+
         try {
-            System.out.println(caver.rpc.klay.getBlockNumber().send().getValue());
-        } catch (IOException e) {
+            caver.wallet.sign(testAddress, accountUpdate);
+            Bytes32 sendResult = caver.rpc.klay.sendRawTransaction(accountUpdate).send();
+
+            if(sendResult.hasError()) {
+                //do something to handle error
+                throw new TransactionException(sendResult.getError().getMessage());
+            }
+            String txHash = sendResult.getResult();
+
+            TransactionReceiptProcessor receiptProcessor = new PollingTransactionReceiptProcessor(caver, 1000, 15);
+            TransactionReceipt.TransactionReceiptData receiptData = receiptProcessor.waitForTransactionReceipt(txHash);
+
+            AccountKey accountKey = caver.rpc.klay.getAccountKey(testAddress).send();
+            System.out.println("Account Key Type: " + accountKey.getResult().getType());
+
+
+            keyring = (SingleKeyring)caver.wallet.updateKeyring(newKeyring);
+
+            ValueTransfer vt = new ValueTransfer.Builder()
+                    .setKlaytnCall(caver.rpc.getKlay())
+                    .setFrom(testAddress)
+                    .setTo(testAddress)
+                    .setValue(BigInteger.valueOf(1))
+                    .setGas(BigInteger.valueOf(25000))
+                    .build();
+
+            caver.wallet.sign(testAddress, vt);
+
+            Bytes32 vtResult = caver.rpc.klay.sendRawTransaction(vt).send();
+            TransactionReceipt.TransactionReceiptData vtReceiptData = receiptProcessor.waitForTransactionReceipt(vtResult.getResult());
+            System.out.println(vtReceiptData.getStatus());
+
+        } catch (IOException | TransactionException e) {
+            // do something to handle exception.
             e.printStackTrace();
         }
     }
