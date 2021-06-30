d55a2d8c7890ee07ddc62234a65b97efc263239b
Denver <aeharvlee@gmail.com>
2021-06-30 19:12:31 +0900

    Update build gradle template

--- transaction/send_klay_with_keystore_file/src/main/java/Boilerplate.java
+++ transaction/send_klay_with_keystore_file/src/main/java/Boilerplate.java
@@ -28,7 +28,7 @@ import java.util.List;
  * Related article - Korean:
  * Related article - English:
  */
-public class BoilerPlate {
+public class Boilerplate {
     // You can directly input values for the variables below, or you can enter values in the caver-java-boilerplate/.env file.
     private static String nodeApiUrl = ""; // e.g. "https://node-api.klaytnapi.com/v1/klaytn";
     private static String accessKeyId = ""; // e.g. "KASK1LVNO498YT6KJQFUPY8S";
