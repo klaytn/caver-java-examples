4a28711fcf8546407815c72b3059e5a0322f3d03
Denver <aeharvlee@gmail.com>
2021-07-06 14:47:19 +0900

    Chore: format codes

--- transaction/send_klay_with_keystore_file/src/main/java/Boilerplate.java
+++ transaction/send_klay_with_keystore_file/src/main/java/Boilerplate.java
@@ -28,11 +28,15 @@ public class Boilerplate {
     private static String accessKeyId = ""; // e.g. "KASK1LVNO498YT6KJQFUPY8S";
     private static String secretAccessKey = ""; // e.g. "aP/reVYHXqjw3EtQrMuJP4A3/hOb69TjnBT3ePKG";
     private static String chainId = ""; // e.g. "1001" or "8217";
-    private static String recipientAddress= ""; // e.g. "0xeb709d59954f4cdc6b6f3bfcd8d531887b7bd199"
+    private static String recipientAddress = ""; // e.g. "0xeb709d59954f4cdc6b6f3bfcd8d531887b7bd199"
 
     public static void main(String[] args) {
-        loadEnv();
-        run();
+        try {
+            loadEnv();
+            run();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public static String objectToString(Object value) throws JsonProcessingException {
@@ -42,7 +46,7 @@ public class Boilerplate {
 
     public static void loadEnv() {
         Dotenv env = Dotenv.configure().directory("../../").ignoreIfMalformed().ignoreIfMissing().load();
-        if (env.get("NODE_API_URL") == null) {
+        if(env.get("NODE_API_URL") == null) {
             // This handle the situation when user tries to run BoilerPlate code from project root directory
             env = Dotenv.configure().directory(System.getProperty("user.dir")).ignoreIfMalformed().ignoreIfMissing().load();
         }
@@ -54,34 +58,33 @@ public class Boilerplate {
         recipientAddress = recipientAddress.equals("") ? env.get("RECIPIENT_ADDRESS") : recipientAddress;
     }
 
-    public static void run() {
-        try {
-            HttpService httpService = new HttpService(nodeApiUrl);
-            httpService.addHeader("Authorization", Credentials.basic(accessKeyId, secretAccessKey));
-            httpService.addHeader("x-chain-id", chainId);
+    public static void run() throws Exception {
+        HttpService httpService = new HttpService(nodeApiUrl);
+        httpService.addHeader("Authorization", Credentials.basic(accessKeyId, secretAccessKey));
+        httpService.addHeader("x-chain-id", chainId);
 
-            Caver caver = new Caver(httpService);
+        Caver caver = new Caver(httpService);
 
-            // 1. Create your own keystore file at "https://baobab.wallet.klaytn.com/create"
-            //    and place the file at `caver-java-examples/transaction/send_klay_with_keystore_file/resources`.
-            // 2. Get 5 KLAY at "https://baobab.wallet.klaytn.com/faucet".
-            File file = new File("resources/keystore.json");
-            if (file.exists() == false) {
-                // Handles when you run this Boilerplate as sub-module using IDE.
-                file = new File("transaction/send_klay_with_keystore_file/resources/keystore.json");
-                if (file.exists() == false) {
-                    throw new Exception("Cannot find keystore.json file.");
-                }
+        // 1. Create your own keystore file at "https://baobab.wallet.klaytn.com/create"
+        //    and place the file at `caver-java-examples/transaction/send_klay_with_keystore_file/resources`.
+        // 2. Get 5 KLAY at "https://baobab.wallet.klaytn.com/faucet".
+        File file = new File("resources/keystore.json");
+        if(file.exists() == false) {
+            // Handles when you run this Boilerplate as sub-module using IDE.
+            file = new File("transaction/send_klay_with_keystore_file/resources/keystore.json");
+            if(file.exists() == false) {
+                throw new Exception("Cannot find keystore.json file.");
             }
-            String password = "Password!@#4"; // Put your password here.
-            ObjectMapper objectMapper = ObjectMapperFactory.getObjectMapper();
-            KeyStore keyStore = objectMapper.readValue(file, KeyStore.class);
-            AbstractKeyring keyring = caver.wallet.keyring.decrypt(keyStore, password);
-            caver.wallet.add(keyring);
+        }
+        String password = "Password!@#4"; // Put your password here.
+        ObjectMapper objectMapper = ObjectMapperFactory.getObjectMapper();
+        KeyStore keyStore = objectMapper.readValue(file, KeyStore.class);
+        AbstractKeyring keyring = caver.wallet.keyring.decrypt(keyStore, password);
+        caver.wallet.add(keyring);
 
-            // Send 1 KLAY.
-            ValueTransfer vt = caver.transaction.valueTransfer.create(
-                    TxPropertyBuilder.valueTransfer()
+        // Send 1 KLAY.
+        ValueTransfer vt = caver.transaction.valueTransfer.create(
+                TxPropertyBuilder.valueTransfer()
                         .setFrom(keyring.getAddress())
                         .setTo(recipientAddress)
                         .setValue(new BigInteger(
@@ -91,12 +94,9 @@ public class Boilerplate {
                                 )
                         ))
                         .setGas(BigInteger.valueOf(25000))
-            );
-            caver.wallet.sign(keyring.getAddress(), vt);
-            Bytes32 result = caver.rpc.klay.sendRawTransaction(vt).send();
-            System.out.println(objectToString(result));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        );
+        caver.wallet.sign(keyring.getAddress(), vt);
+        Bytes32 result = caver.rpc.klay.sendRawTransaction(vt).send();
+        System.out.println(objectToString(result));
     }
 }
