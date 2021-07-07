e43cc7c5e9594914ff5cfdeb4a8b1ec4cd40958a
Denver <aeharvlee@gmail.com>
2021-07-07 09:40:52 +0900

    Update boilerplate

--- wallet/sign_transaction/src/main/java/Boilerplate.java
+++ wallet/sign_transaction/src/main/java/Boilerplate.java
@@ -2,33 +2,21 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import com.klaytn.caver.Caver;
-import com.klaytn.caver.account.Account;
-import com.klaytn.caver.account.WeightedMultiSigOptions;
-import com.klaytn.caver.methods.response.AccountKey;
-import com.klaytn.caver.methods.response.Bytes32;
-import com.klaytn.caver.methods.response.TransactionReceipt;
 import com.klaytn.caver.transaction.TxPropertyBuilder;
-import com.klaytn.caver.transaction.response.PollingTransactionReceiptProcessor;
-import com.klaytn.caver.transaction.response.TransactionReceiptProcessor;
-import com.klaytn.caver.transaction.type.AccountUpdate;
 import com.klaytn.caver.transaction.type.ValueTransfer;
 import com.klaytn.caver.utils.Utils;
-import com.klaytn.caver.wallet.keyring.RoleBasedKeyring;
 import com.klaytn.caver.wallet.keyring.SingleKeyring;
 import io.github.cdimascio.dotenv.Dotenv;
 import okhttp3.Credentials;
-import org.web3j.protocol.exceptions.TransactionException;
 import org.web3j.protocol.http.HttpService;
 
 import java.math.BigDecimal;
 import java.math.BigInteger;
-import java.util.Arrays;
-import java.util.List;
 
 /**
  * Boilerplate code about "How to sign transaction using Wallet"
- * Related reference - Korean: https://docs.klaytn.com/bapp/sdk/caver-java/getting-started#sending-a-value-transfer-transaction
- * Related reference - English: https://ko.docs.klaytn.com/bapp/sdk/caver-java/getting-started#sending-a-value-transfer-transaction
+ * Related reference - Korean: https://ko.docs.klaytn.com/bapp/sdk/caver-java/getting-started#sending-a-value-transfer-transaction
+ * Related reference - English: https://docs.klaytn.com/bapp/sdk/caver-java/getting-started#sending-a-value-transfer-transaction
  */
 public class Boilerplate {
     // You can directly input values for the variables below, or you can enter values in the caver-java-boilerplate/.env file.
@@ -36,13 +24,17 @@ public class Boilerplate {
     private static String accessKeyId = ""; // e.g. "KASK1LVNO498YT6KJQFUPY8S";
     private static String secretAccessKey = ""; // e.g. "aP/reVYHXqjw3EtQrMuJP4A3/hOb69TjnBT3ePKG";
     private static String chainId = ""; // e.g. "1001" or "8217";
-    private static String senderAddress= ""; // e.g. "0xeb709d59954f4cdc6b6f3bfcd8d531887b7bd199"
+    private static String senderAddress = ""; // e.g. "0xeb709d59954f4cdc6b6f3bfcd8d531887b7bd199"
     private static String senderPrivateKey = ""; // e.g. "0x42f6375b608c2572fadb2ed9fd78c5c456ca3aa860c43192ad910c3269727fc7"
-    private static String recipientAddress= ""; // e.g. "0xeb709d59954f4cdc6b6f3bfcd8d531887b7bd199"
+    private static String recipientAddress = ""; // e.g. "0xeb709d59954f4cdc6b6f3bfcd8d531887b7bd199"
 
     public static void main(String[] args) {
-        loadEnv();
-        run();
+        try {
+            loadEnv();
+            run();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public static String objectToString(Object value) throws JsonProcessingException {
@@ -52,7 +44,7 @@ public class Boilerplate {
 
     public static void loadEnv() {
         Dotenv env = Dotenv.configure().directory("../../").ignoreIfMalformed().ignoreIfMissing().load();
-        if (env.get("NODE_API_URL") == null) {
+        if(env.get("NODE_API_URL") == null) {
             // This handle the situation when user tries to run BoilerPlate code from project root directory
             env = Dotenv.configure().directory(System.getProperty("user.dir")).ignoreIfMalformed().ignoreIfMissing().load();
         }
@@ -66,40 +58,31 @@ public class Boilerplate {
         recipientAddress = recipientAddress.equals("") ? env.get("RECIPIENT_ADDRESS") : recipientAddress;
     }
 
-    public static void run() {
-        try {
-            HttpService httpService = new HttpService(nodeApiUrl);
-            if (accessKeyId.isEmpty() || secretAccessKey.isEmpty()) {
-                throw new Exception("accessKeyId and secretAccessKey must not be empty.");
-            }
-            httpService.addHeader("Authorization", Credentials.basic(accessKeyId, secretAccessKey));
-            httpService.addHeader("x-chain-id", chainId);
+    public static void run() throws Exception {
+        HttpService httpService = new HttpService(nodeApiUrl);
+        httpService.addHeader("Authorization", Credentials.basic(accessKeyId, secretAccessKey));
+        httpService.addHeader("x-chain-id", chainId);
+        Caver caver = new Caver(httpService);
 
-            Caver caver = new Caver(httpService);
+        SingleKeyring senderKeyring = caver.wallet.keyring.create(senderAddress, senderPrivateKey);
 
-            SingleKeyring senderKeyring = caver.wallet.keyring.create(senderAddress, senderPrivateKey);
-
-            // Create ValueTransfer transaction sending 1 KLAY to recipientAddress
-            ValueTransfer vt = caver.transaction.valueTransfer.create(
-                    TxPropertyBuilder.valueTransfer()
+        // Create ValueTransfer transaction sending 1 KLAY to recipientAddress
+        ValueTransfer vt = caver.transaction.valueTransfer.create(
+                TxPropertyBuilder.valueTransfer()
                         .setFrom(senderKeyring.getAddress())
                         .setTo(recipientAddress)
                         .setValue(caver.utils.convertToPeb(BigDecimal.valueOf(1), Utils.KlayUnit.KLAY))
                         .setGas(BigInteger.valueOf(25000))
-            );
+        );
+        System.out.println("Before sign the transaction using wallet, there is no signatures.");
+        System.out.println(objectToString(vt.getSignatures())); // Empty siganture
 
-            System.out.println("Before sign the transaction using wallet, there is no signatures.");
-            System.out.println(objectToString(vt.getSignatures())); // Empty siganture
-
-            // Signing process
-            System.out.println("Add a senderKeyring to `caver.wallet`.");
-            caver.wallet.add(senderKeyring);
-            System.out.println("Sign the transaction using `caver.wallet`.");
-            caver.wallet.sign(senderKeyring.getAddress(), vt);
-            System.out.println("Signature was added, so we can see the signature of sender.");
-            System.out.println(objectToString(vt.getSignatures())); // Must contains signature
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        // Signing process
+        System.out.println("Add a senderKeyring to `caver.wallet`.");
+        caver.wallet.add(senderKeyring);
+        System.out.println("Sign the transaction using `caver.wallet`.");
+        caver.wallet.sign(senderKeyring.getAddress(), vt);
+        System.out.println("Signature was added, so we can see the signature of sender.");
+        System.out.println(objectToString(vt.getSignatures())); // Must contains signature
     }
 }
