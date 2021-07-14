b00cd14adf40f51fb04e1f93a3410057fc0b4803
Denver <aeharvlee@gmail.com>
2021-07-14 17:30:40 +0900

    Update hello world example
    Made it simple to make possible user test themselves.

--- hello_world/src/main/java/CaverExample.java
+++ hello_world/src/main/java/CaverExample.java
@@ -56,52 +56,15 @@ public class CaverExample {
     }
 
     public static void run() throws Exception {
-        System.out.println("In the caver, almost everything starts with the caver. Let's create a caver instance :)");
-
+        // In the caver, almost everything starts with the caver. Let's create a caver instance
         HttpService httpService = new HttpService(nodeApiUrl);
         httpService.addHeader("Authorization", Credentials.basic(accessKeyId, secretAccessKey));
         httpService.addHeader("x-chain-id", chainId);
         Caver caver = new Caver(httpService);
 
-        System.out.println("Now that we have created the caver instance, we can do whatever we want with the caver.");
-        System.out.println("Let's test codes at https://docs.klaytn.com/bapp/sdk/caver-java/getting-started#sending-klay-at-a-glance just for testing.");
-        System.out.println();
-
-        String password = "password";
-        String keyStoreJsonString = "{\n" +
-                "  \"version\": 4,\n" +
-                "  \"id\": \"9c12de05-0153-41c7-a8b7-849472eb5de7\",\n" +
-                "  \"address\": \"0xc02cec4d0346bf4124deeb55c5216a4138a40a8c\",\n" +
-                "  \"keyring\": [\n" +
-                "    {\n" +
-                "      \"ciphertext\": \"eacf496cea5e80eca291251b3743bf93cdbcf7072efc3a74efeaf518e2796b15\",\n" +
-                "      \"cipherparams\": {\n" +
-                "        \"iv\": \"d688a4319342e872cefcf51aef3ec2da\"\n" +
-                "      },\n" +
-                "      \"cipher\": \"aes-128-ctr\",\n" +
-                "      \"kdf\": \"scrypt\",\n" +
-                "      \"kdfparams\": {\n" +
-                "        \"dklen\": 32,\n" +
-                "        \"salt\": \"c3cee502c7157e0faa42386c6d666116ffcdf093c345166c502e23bc34e6ba40\",\n" +
-                "        \"n\": 4096,\n" +
-                "        \"r\": 8,\n" +
-                "        \"p\": 1\n" +
-                "      },\n" +
-                "      \"mac\": \"4b49574f3d3356fa0d04f73e07d5a2a6bbfdd185bedfa31f37f347bc98f2ef26\"\n" +
-                "    }\n" +
-                "  ]\n" +
-                "}";
-
-        SingleKeyring decrypt = (SingleKeyring)caver.wallet.keyring.decrypt(keyStoreJsonString, password);
-        System.out.println("Decrypted address : " + decrypt.getAddress());
-        System.out.println("Decrypted key : " + decrypt.getKey().getPrivateKey());
-
-        SingleKeyring addedKeyring = (SingleKeyring)caver.wallet.add(decrypt);
-        System.out.println("address : " + addedKeyring.getAddress());
-        System.out.println("key : " + addedKeyring.getKey().getPrivateKey());
-        System.out.println();
-
-        System.out.println("OK! It works :) You can test another codes whatever you want :)");
-        System.out.println("Or check some scenarios in this caver-java-examples repository.");
+        // Now that we have created the caver instance, we can do whatever we want with the caver.
+        // Write your own code below using Caver instance :)
+        SingleKeyring singleKeyring = caver.wallet.keyring.generate();
+        System.out.println(objectToString(singleKeyring));
     }
 }
